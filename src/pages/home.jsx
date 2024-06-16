bbd3bae4c970a4295b325df4f776e302e49b2dc7
Leandro Celle (WPP Group USA LLC) <v-lcelle@microsoft.com>
2024-06-16 17:06:11 -0300

    Modify styles

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,7 +7,7 @@ import Header from '../components/header';
 function Home() {
   return (
     <>
-      <div>
+      <div className='body'>
         <Header />
         <Products />
         <Navbar />
