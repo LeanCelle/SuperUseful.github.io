84fb0bf41dc1b4b9b914083126eaf7cb85dce196
Leandro Celle (WPP Group USA LLC) <v-lcelle@microsoft.com>
2024-06-16 16:21:27 -0300

    Modify styles

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../sass/homestyle.css'; // Importa los estilos CSS
+
+function Header() {
+  return (
+    <>
+      <div>
+        <h1 className="header">SuperUseful!</h1>
+      </div>
+    </>
+  );
+}
+
+export default Header;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import '../sass/homestyle.css'; // Importa los estilos CSS
 import Navbar from '../components/navbar';
 import Products from '../components/products';
+import Header from '../components/header';
 
 function Home() {
   return (
     <>
       <div>
-        <h1 className="header">SuperUseful!</h1>
+        <Header />
         <Products />
         <Navbar />
       </div>
