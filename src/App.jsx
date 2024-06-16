0318ee0a52fc443e59742de6b0ea2f09f04ab8b8
Leandro Celle (WPP Group USA LLC) <v-lcelle@microsoft.com>
2024-06-16 20:58:50 -0300

    Scroll modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '../src/sass/homestyle.css'; // Importa el archivo de estilos CSS global
-import Home from './pages/home';// Cambiado 'home' a 'Home' para seguir las convenciones de nombres
+import Home from './pages/home'; // Ajustado para seguir convenciones de nombres
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  useEffect(() => {
+    // Al montar el componente, desactiva el scroll del body
+    document.body.style.overflow = 'hidden';
+
+    // Al desmontar el componente, restaura el scroll del body
+    return () => {
+      document.body.style.overflow = 'visible';
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
@@ -15,5 +25,6 @@ function App() {
 }
 
 export default App;
+
 
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,9 @@ import '../sass/homestyle.css'; // Importa los estilos CSS
 function Header() {
   return (
     <>
+      <div className='headerContainer'>
         <h1 className="header">SuperUseful!</h1>
+      </div>
     </>
   );
 }
