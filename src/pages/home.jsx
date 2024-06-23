95547ca3bce9751e37f51eb478c35c53a0397b57
Leandro Celle (WPP Group USA LLC) <v-lcelle@microsoft.com>
2024-06-23 18:11:26 -0300

    Create Modal component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../sass/homestyle.css'; // Importa los estilos CSS
 import Navbar from '../components/navbar';
 import Products from '../components/products';
 import Header from '../components/header';
+import StaticExample from '../components/modal';
 
 function Home() {
+  const [showModal, setShowModal] = useState(true); // Estado para controlar la visibilidad del modal
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
       <div className='body'>
@@ -12,9 +19,9 @@ function Home() {
         <Products />
         <Navbar />
       </div>
+      {showModal && <StaticExample onClose={closeModal} />} {/* Renderiza el modal si showModal es true */}
     </>
   );
 }
 
 export default Home;
-
